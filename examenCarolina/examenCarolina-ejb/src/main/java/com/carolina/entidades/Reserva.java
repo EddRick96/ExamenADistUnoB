9d19956640a3eaf3b9361af388245005ca8da688
dennisNunez997 <dennis.nunez@epn.edu.ec>
2020-07-23 16:04:15 -0500

    entidades

---
+++ examenCarolina/examenCarolina-ejb/src/main/java/com/carolina/entidades/Reserva.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.carolina.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author dennis
+ */
+@Entity
+@Table(name = "reserva")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Reserva.findAll", query = "SELECT r FROM Reserva r")
+    , @NamedQuery(name = "Reserva.findById", query = "SELECT r FROM Reserva r WHERE r.id = :id")
+    , @NamedQuery(name = "Reserva.findByNombre", query = "SELECT r FROM Reserva r WHERE r.nombre = :nombre")
+    , @NamedQuery(name = "Reserva.findByApellido", query = "SELECT r FROM Reserva r WHERE r.apellido = :apellido")
+    , @NamedQuery(name = "Reserva.findByCancha", query = "SELECT r FROM Reserva r WHERE r.cancha = :cancha")
+    , @NamedQuery(name = "Reserva.findByFecha", query = "SELECT r FROM Reserva r WHERE r.fecha = :fecha")
+    , @NamedQuery(name = "Reserva.findByHora", query = "SELECT r FROM Reserva r WHERE r.hora = :hora")
+    , @NamedQuery(name = "Reserva.findByDescripcion", query = "SELECT r FROM Reserva r WHERE r.descripcion = :descripcion")})
+public class Reserva implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "cancha")
+    private String cancha;
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "hora")
+    private String hora;
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    public Reserva() {
+    }
+
+    public Reserva(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCancha() {
+        return cancha;
+    }
+
+    public void setCancha(String cancha) {
+        this.cancha = cancha;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Reserva)) {
+            return false;
+        }
+        Reserva other = (Reserva) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.carolina.entidades.Reserva[ id=" + id + " ]";
+    }
+    
+}
